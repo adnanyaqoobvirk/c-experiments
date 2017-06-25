fd38424a93a3792958b3d42ab291a09ab4dfcb0e
Adnan Yaqoob <adnan@yaqoob.org>
2017-06-25 17:37:19 +0800

    adds vscode project configuration

---
+++ syntax_learning/hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+   /* my first program in C */
+   int first_var = 20;
+
+   printf("First number print: %d \n", first_var);
+   printf("Why it is not printing the above message \n");
+   
+   return 0;
+}
